bb7b0bf06d3a89080d35c0c4d215662c6c34b217
Edward Woolhouse <danishcake@hotmail.com>
2025-05-28 15:14:40 +0100

    feat: allow CARGO_REGISTRY_TOKEN verification to be skipped
    To skip, both publish and new option 'alwaysVerifyToken' must be false

--- src/PluginConfig.fs
+++ src/PluginConfig.fs
@@ -7,4 +7,5 @@ type PluginConfig =
         abstract checkArgs: string array option with get
         abstract publish: bool option with get
         abstract publishArgs: string array option with get
+        abstract alwaysVerifyToken: bool option with get
     end
